57e50947395239d39868974b860484405d883024
Christian <chris.mood92@gmail.com>
2020-09-30 16:25:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 function isPalindrome(text){
   
-  if (text.length === 1) return true
+  if (text.length === 1) return true;
   
-  let repeats = {}
+  let repeats = {};
   
   for (let i = 0; i < text.length; i++){
-   	let character = text.charAt(i)
+    let character = text.charAt(i);
     repeats[character] = (isNaN(repeats[character]) ? 1 : repeats[character] + 1);
   }
   
-  const listRepeats = Object.values(repeats)
+  const listRepeats = Object.values(repeats);
   
-  let countEvens = 0
-  let countOdds = 0
+  let countEvens = 0;
+  let countOdds = 0;
   
   for (let j = 0; j < listRepeats.length; j++){
     if (listRepeats[j] % 2 === 0) countEvens++;
     if (listRepeats[j] % 2 === 1) countOdds++;
   }
-    
+  
   return countOdds <= 1 && countEvens > 0;
 }
